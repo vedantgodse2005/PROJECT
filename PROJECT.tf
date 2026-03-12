74b15f45ef8e099cf6e6241041be6dfc4e133789
vedantgodse2005 <vedantgodse@gmail.com>
2026-03-12 09:55:49 +0530

    Refactor Terraform configuration for AWS infrastructure

--- PROJECT.tf
+++ PROJECT.tf
@@ -1,111 +1,188 @@
-variable "region" {
-  default = "us-east-1"
+provider "aws" {
+    region = var.region
+  
 }
 
-variable "mumbai_vpc_cidr" {
-  description = "CIDR of Vpc"
-  type = string
-  default = "192.78.0.0/16"
+resource "aws_vpc" "my-vpc" {
+  cidr_block = var.mumbai_vpc_cidr
+  tags = {
+    Name= var.vpc_name
+  }
 }
 
-variable "vpc_name" {
-  description = "Vpc Name"
-  type = string
-  default = "My-vpc"
+resource "aws_subnet" "mysubnet-1" {
+  vpc_id = aws_vpc.my-vpc.id
+  cidr_block = var.public_cidr_block
+  map_public_ip_on_launch = true
+  availability_zone = var.public_available_zone
+  tags = {
+    Name = var.public_subnet_name
+  }
 }
 
-variable "public_cidr_block" {
-    description = "Public Subnet CIDR block"
-    type = string
-    default = "192.78.0.0/20"
+resource "aws_subnet" "mysubnet-2" {
+  vpc_id = aws_vpc.my-vpc.id
+  cidr_block = var.private_cidr_block
+  availability_zone = var.private_available_zone
+  tags = {
+    Name = var.private_subnet_name
+  }
 }
 
-variable "public_available_zone" {
-    description = "Public subnet availability Zone"
-    type = string
-    default = "us-east-1"
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my-vpc.id
+
+  tags = {
+    Name = var.igw_name
+  }
 }
 
-variable "public_subnet_name" {
-    description = "Public subnet name"
-    type = string
-    default = "public-subnet"
+resource "aws_default_route_table" "default-tb" {
+  default_route_table_id = aws_vpc.my-vpc.default_route_table_id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
 }
 
-variable "private_cidr_block" {
-    description = "Private CIDR Block"
-    type = string
-    default = "192.78.16.0/20"
+resource "aws_route_table_association" "public-assoc" {
+  subnet_id = aws_subnet.mysubnet-1.id
+  route_table_id = aws_default_route_table.default-tb.id
 }
 
-variable "private_available_zone" {
-  description = "Private subnet availability zone"
-  type = string
-  default = "us-east-1"
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
 }
 
-variable "private_subnet_name" {
-    description = "private subnet name"
-    type = string
-    default = "private-subnet"
+resource "aws_nat_gateway" "my-ngw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.mysubnet-1.id
+
+  tags = {
+    Name = var.nat_name
+  }
 }
 
-variable "igw_name" {
-  description = "Internat Gateway Name"
-  type = string
-  default = "my-igw"
+resource "aws_route_table" "NAT-tb" {
+  vpc_id = aws_vpc.my-vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.my-ngw.id
+  }
+
+  tags = {
+    Name = var.nat_route_table_name
+  }
 }
 
-variable "nat_name" {
-    description = "NAT Gateway name"
-    type = string
-    default = "my-ngw"
+resource "aws_route_table_association" "private-assoc" {
+  subnet_id = aws_subnet.mysubnet-2.id
+  route_table_id = aws_route_table.NAT-tb.id
+  
 }
 
-variable "nat_route_table_name" {
-    description = "NAT Gateway route table name"
-    type = string
-    default = "NAT-tb"
+resource "aws_security_group" "my-sg-1" {
+  name        = var.security_group_name
+  description = var.description_sg
+  vpc_id = aws_vpc.my-vpc.id
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 3306
+    to_port     = 3306
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 }
 
-variable "security_group_name" {
- description = "Name of security group" 
- type = string
- default = "My-sg-1"
+resource "aws_instance" "Ec2Instance" {
+    ami           = var.image_instance
+    instance_type = var.instance_type
+    key_name = var.instance_key
+    vpc_security_group_ids = [aws_security_group.my-sg-1.id]
+    subnet_id = aws_subnet.mysubnet-1.id
+    user_data = <<-EOF
+    #!/bin/bash
+    yum install java -y
+    curl -O https://dlcdn.apache.org/tomcat/tomcat-9/v9.0.115/bin/apache-tomcat-9.0.115.tar.gz
+    tar -xzvf apache-tomcat-9.0.115.tar.gz -C /opt
+    /opt/apache-tomcat-9.0.115/bin/./catalina.sh start
+    cd /opt/apache-tomcat-9.0.115/webapps/
+    curl -O https://s3-us-west-2.amazonaws.com/studentapi-cit/student.war
+    cd /opt/apache-tomcat-9.0.115/lib/
+    curl -O https://s3-us-west-2.amazonaws.com/studentapi-cit/mysql-connector.jar
+    python3 -c "
+    f = open('/opt/apache-tomcat-9.0.115/conf/context.xml', 'r')
+    lines = f.readlines()
+    f.close()
+    resource = '    <Resource name=\"jdbc/TestDB\" auth=\"Container\" type=\"javax.sql.DataSource\" maxTotal=\"500\" maxIdle=\"30\" maxWaitMillis=\"1000\" username=\"arya\" password=\"Aryakadam47\" driverClassName=\"com.mysql.jdbc.Driver\" url=\"jdbc:mysql://${aws_db_instance.my_db.address}:3306/studentapp?useUnicode=yes&amp;characterEncoding=utf8\"/>\n'
+    lines.insert(-1, resource)
+    f = open('/opt/apache-tomcat-9.0.115/conf/context.xml', 'w')
+    f.writelines(lines)
+    f.close()
+    "
+    /opt/apache-tomcat-9.0.115/bin/./catalina.sh stop
+    /opt/apache-tomcat-9.0.115/bin/./catalina.sh start
+    EOF
+    tags = {
+      Name = var.public_instance_name
+    }
+}
+resource "aws_instance" "db-instance" {
+    ami           = var.image_instance
+    instance_type = var.instance_type
+    key_name = var.instance_key
+    vpc_security_group_ids = [aws_security_group.my-sg-1.id]
+    subnet_id = aws_subnet.mysubnet-1.id
+    user_data = <<-EOF
+    #!/bin/bash
+    yum install mariadb105* -y
+    systemctl start mariadb.service
+    systemctl enable mariadb.service
+
+    mysql -h ${aws_db_instance.my_db.address} -u arya -pAryakadam47 <<MYSQL
+    create database studentapp;
+    use studentapp;
+    CREATE TABLE if not exists students(student_id INT NOT NULL AUTO_INCREMENT,
+	    student_name VARCHAR(100) NOT NULL,
+      student_addr VARCHAR(100) NOT NULL,
+  	  student_age VARCHAR(3) NOT NULL,
+	    student_qual VARCHAR(20) NOT NULL,
+	    student_percent VARCHAR(10) NOT NULL,
+  	  student_year_passed VARCHAR(10) NOT NULL,
+	    PRIMARY KEY (student_id)
+	  );
+MYSQL
+    EOF
+    tags = {
+      Name = var.private_instance_name
+    }
 }
 
-variable "description_sg" {
-    description = "Description of Security Group"
-    type = string
-    default = "Allow SSH , HTTP and HTTPS traffic"
-}
 
-variable "image_instance" {
-    description = "AMI of ec2 instance"
-    type = string
-    default = "ami-02dfbd4ff395f2a1b"
-}
-
-variable "instance_type" {
-  description = "Instance Type"
-  type = string
-  default = "t3.micro"
-}
-
-variable "instance_key" {
-  description = "Key pair"
-  type = string
-  default = "VEDANT1"
-}
-
-variable "public_instance_name" {
-    description = "Public Instance name"
-    type = string
-    default = "jump-server"
-}
-
-variable "private_instance_name" {
-  description = "Private instance name"
-  type = string
-  default = "application-server"
+output "public-ip" {
+  value = aws_instance.Ec2Instance.public_ip
 }
